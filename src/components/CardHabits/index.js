dc7890ed6070e08d8c57978e74a5061d61974a91
Victor Scherer <victor_scherer@hotmail.com>
2021-10-18 09:35:04 -0300

    Fit: CardHabits create

---
+++ src/components/CardHabits/index.js
@@ -0,0 +1,27 @@
+import { ButtonContainer, Container, DataButtonDiv, DataContainer, NameContainer, TrashButton } from './styled'
+import trash from "../../img/trash.png";
+
+const CardHabits = ({ id, categoria, dificuldade, frequencia }) => {
+
+    return (
+        <Container>
+            <NameContainer>
+                <p>Habito {id}</p>
+            </NameContainer>
+            <DataButtonDiv>
+                <DataContainer>
+                    <p>Categoria: {categoria}</p>
+                    <p>Dificuldade: {dificuldade}</p>
+                    <p>Frequencia: {frequencia}</p>
+                </DataContainer>
+
+                <ButtonContainer>
+                    <TrashButton src={trash} alt="Lixo" />
+                    <input type="checkbox" ></input>
+                </ButtonContainer>
+            </DataButtonDiv>
+        </Container>
+    )
+}
+
+export default CardHabits;
