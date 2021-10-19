bcc081d9b4d5493c6c2fe8cfe8da98b60cc0e79d
emanuel <emanuelzm@protonmail.com>
2021-10-19 16:39:47 -0300

    feat: add sidebar routes

--- src/components/CardHabits/index.js
+++ src/components/CardHabits/index.js
@@ -1,43 +1,43 @@
 import {
-  ButtonContainer,
-  Container,
-  DataButtonDiv,
-  DataContainer,
-  NameContainer,
-  TrashButton,
-} from "./styles";
-import { BsTrash } from "react-icons/bs";
-import { useAuth } from "../../providers/auth";
-import { Redirect, useHistory } from "react-router-dom";
+	ButtonContainer,
+	DataButtonDiv,
+	DataContainer,
+	NameContainer,
+	TrashButton,
+} from './styles';
+import { BsTrash } from 'react-icons/bs';
+import { useAuth } from '../../providers/auth';
+import { Redirect, useHistory } from 'react-router-dom';
+import { Template } from '../Template';
 
 const CardHabits = ({ id, categoria, dificuldade, frequencia }) => {
-  const { auth } = useAuth();
-  const history = useHistory();
-  if (!auth) {
-    history.push("/");
-    return <Redirect to="/" />;
-  }
-  return (
-    <Container>
-      <NameContainer>
-        <p>Habito {id}</p>
-      </NameContainer>
-      <DataButtonDiv>
-        <DataContainer>
-          <p>Categoria: {categoria}</p>
-          <p>Dificuldade: {dificuldade}</p>
-          <p>Frequencia: {frequencia}</p>
-        </DataContainer>
+	const { auth } = useAuth();
+	const history = useHistory();
+	if (!auth) {
+		history.push('/');
+		return <Redirect to="/" />;
+	}
+	return (
+		<Template>
+			<NameContainer>
+				<p>Habito {id}</p>
+			</NameContainer>
+			<DataButtonDiv>
+				<DataContainer>
+					<p>Categoria: {categoria}</p>
+					<p>Dificuldade: {dificuldade}</p>
+					<p>Frequencia: {frequencia}</p>
+				</DataContainer>
 
-        <ButtonContainer>
-          <TrashButton>
-            <BsTrash />
-          </TrashButton>
-          <input type="checkbox"></input>
-        </ButtonContainer>
-      </DataButtonDiv>
-    </Container>
-  );
+				<ButtonContainer>
+					<TrashButton>
+						<BsTrash />
+					</TrashButton>
+					<input type="checkbox"></input>
+				</ButtonContainer>
+			</DataButtonDiv>
+		</Template>
+	);
 };
 
 export default CardHabits;
--- src/components/PermanentSidebar/styles.js
+++ src/components/PermanentSidebar/styles.js
@@ -1,61 +1,61 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import {
-  Box,
-  Drawer,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@material-ui/core";
+	Box,
+	Drawer,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+} from '@material-ui/core';
 
 export const Container = styled(Box)`
-  display: flex;
+	display: flex;
 `;
 
 export const Sidebar = styled(Drawer)`
-  flex-shrink: 0;
-  @media (max-width: 1024px) {
-    display: none;
-  }
+	flex-shrink: 0;
+	@media (max-width: 1024px) {
+		display: none;
+	}
 `;
 
 export const SidebarItems = styled(Box)`
-  overflow: auto;
-  width: 260px;
+	overflow: auto;
+	width: 260px;
 `;
 
 export const UserField = styled.div`
-  height: 250px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  svg,
-  svg path {
-    font-size: 70px;
-  }
+	height: 250px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	svg,
+	svg path {
+		font-size: 70px;
+	}
 `;
 
 export const RouteBlock = styled(ListItem)`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  width: 341px;
-  height: 106px;
-  padding: 0 !important;
-  border-bottom: 1px solid lightgray !important;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	width: 341px;
+	height: 106px;
+	padding: 0 !important;
+	border-bottom: 1px solid lightgray !important;
 `;
 
-export const Icon = styled(ListItemIcon)`
-  font-size: 30px;
-  margin: 0 1em;
-  svg,
-  svg path {
-    color: var(--black);
-  }
+export const IconBox = styled(ListItemIcon)`
+	font-size: 30px;
+	margin: 0 1em;
+	svg,
+	svg path {
+		color: var(--black);
+	}
 `;
 
 export const RouteItem = styled(ListItemText)`
-  font-size: 14px;
+	font-size: 14px;
 `;
 
 export const Username = styled.h4``;
