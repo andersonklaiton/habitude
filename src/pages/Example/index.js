1f0118f2ef575eb17e46b7a0af3ff07225b29b91
AndersonS <klaito.b@gmail.com>
2021-10-19 09:48:27 -0300

    add header and sidebar

---
+++ src/pages/Example/index.js
@@ -0,0 +1,11 @@
+import { Template } from '../../components/Template';
+
+const Example = () => {
+	return (
+		<Template>
+			<h1>Hello world!</h1>
+		</Template>
+	);
+};
+
+export default Example;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,25 +1,40 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-    outline:0;
-}
-button{
-    cursor:pointer;
-}
-a{
-    text-decoration:none;
-}
-
-:root{
-    --green:#80A26B;
-    --red:#CA513C;
-    --light-blue:#94B9E1;
-    --blue:#4D98C7;
-    --pink:#FB5793;
-    --yellow:#FDE09A;
-}
+    * {
+        margin:0;
+        padding:0;
+        outline:0;
+        box-sizing:border-box;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
+    }
+    html, body {
+        min-height: 100vh;
+    }
+    button {
+        cursor:pointer;
+        background: none;
+        border: none;
+        :focus {
+            outline: none;
+        }
+    }
+    a {
+        text-decoration:none;
+    }
+    svg,
+	svg path {
+        margin: 0;
+    }
+    :root {
+        --green:#80A26B;
+        --red:#CA513C;
+        --light-blue:#94B9E1;
+        --blue:#4D98C7;
+        --pink:#FB5793;
+        --yellow:#FDE09A;
+        --white: #FFFFFF;
+        --black: #000000;
+    }
 `;
