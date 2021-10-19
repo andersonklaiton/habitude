719ae8074e804f34205a6e24ab87b1d11f80c3bd
Lucas Elias <lucaselias2509@gmail.com>
2021-10-19 10:37:12 -0300

    fix: routes fixed

--- src/providers/auth/index.js
+++ src/providers/auth/index.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import api from "../../services/api";
 import jwt_decode from "jwt-decode";
-import {toast}from "react-toastify"
+import { toast } from "react-toastify";
 
 const AuthContext = createContext();
 
@@ -10,7 +10,6 @@ export const AuthProvider = ({ children }) => {
   const UserName = localStorage.getItem("userName") || "";
   const [auth, setAuth] = useState(token);
   const [username, setUsername] = useState(UserName);
-  
 
   const logIn = (data, history) => {
     api
@@ -22,16 +21,17 @@ export const AuthProvider = ({ children }) => {
         localStorage.setItem("token", JSON.stringify(response.data.access));
         const tokenDecode = jwt_decode(response.data.access);
         setAuth(tokenDecode);
-        history.push("/habits");
-        toast.success(`Logado com ${data.username}`)
-     
-
+        history.push("/dashboard");
+        toast.success(`Logado com ${data.username}`);
       })
-      .catch((error) => toast.error("Usuário não encontrado, tente novamente"),  history.push("/"));
+      .catch(
+        (error) => toast.error("Usuário não encontrado, tente novamente"),
+        history.push("/")
+      );
   };
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth, logIn, username}}>
+    <AuthContext.Provider value={{ auth, setAuth, logIn, username }}>
       {children}
     </AuthContext.Provider>
   );
