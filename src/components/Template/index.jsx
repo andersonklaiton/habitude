cac5130ee4ab587b18e1e2a686ebe2ebca52f82a
emanuel <emanuelzm@protonmail.com>
2021-10-19 08:57:07 -0300

    style: repositioned main tag

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -1,14 +1,14 @@
 import Header from '../Header';
 import PermanentSidebar from '../PermanentSidebar';
 
-import { TemplateContainer } from './styles';
+import { MainContent, TemplateContainer } from './styles';
 
 export const Template = ({ children }) => {
 	return (
 		<TemplateContainer>
 			<Header />
 			<PermanentSidebar />
-			<main>{children}</main>
+			<MainContent>{children}</MainContent>
 		</TemplateContainer>
 	);
 };
