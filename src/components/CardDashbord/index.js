f3c25bf28f92f9fd3cdfee7d30527c45deb69316
Victor Scherer <victor_scherer@hotmail.com>
2021-10-19 16:24:15 -0300

    Fix: Parameters of cardHabits and width of data container

--- src/components/CardDashbord/index.js
+++ src/components/CardDashbord/index.js
@@ -1,18 +1,20 @@
 import { ButtonContainer, Container, DataButtonDiv, DataContainer, NameContainer, TrashButton } from './styles'
 import { BsTrash } from 'react-icons/bs';
 
-const CardHabits = ({ id, categoria, dificuldade, frequencia }) => {
+const CardHabits = ({ habits }) => {
+
+    const { title, category, difficulty, frequency, achieved, how_much_achieved, user } = habits;
 
     return (
         <Container>
             <NameContainer>
-                <p>Habito {id}</p>
+                <p>{title}</p>
             </NameContainer>
             <DataButtonDiv>
                 <DataContainer>
-                    <p>Categoria: {categoria}</p>
-                    <p>Dificuldade: {dificuldade}</p>
-                    <p>Frequencia: {frequencia}</p>
+                    <p>Categoria: {category}</p>
+                    <p>Dificuldade: {difficulty}</p>
+                    <p>Frequencia: {frequency}</p>
                 </DataContainer>
 
                 <ButtonContainer>
--- src/components/CardDashbord/styles.js
+++ src/components/CardDashbord/styles.js
@@ -36,7 +36,7 @@ export const DataContainer = styled.div`
     flex-direction: column;
     justify-content: space-around;
     align-items: flex-start;
-    width: 50%;
+    width: 80%;
     height: 100%;
     color: #A6A6A6;
     margin-left: 5px;
@@ -47,7 +47,7 @@ export const ButtonContainer = styled.div`
     flex-direction: column;
     justify-content: space-around;
     align-items: flex-end;
-    width: 50%;
+    width: 20%;
     height: 100%;
     margin-right: 5px;
 `;
