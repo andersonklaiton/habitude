0ee5585f7b161b08a6bc7f08de802635c90b0e9d
emanuel <emanuelzm@protonmail.com>
2021-10-21 14:48:33 -0300

    feat: add button mobile ver

--- src/pages/HabitsPage/index.js
+++ src/pages/HabitsPage/index.js
@@ -1,51 +1,56 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from 'react';
 
-import { useHistory } from "react-router-dom";
-import CardHabits from "../../components/CardDashbord";
-import { Template } from "../../components/Template";
-import { useAuth } from "../../providers/auth";
+import { useHistory } from 'react-router-dom';
+import CardHabits from '../../components/CardDashbord';
+import { Template } from '../../components/Template';
+import { useAuth } from '../../providers/auth';
 
-import api from "../../services/api";
-import { HabitsContainer } from "./styles";
+import api from '../../services/api';
+import { ButtonAdd, HabitsContainer } from './styles';
 
 const HabitsPage = () => {
-  const history = useHistory();
-  const { auth } = useAuth();
+	const history = useHistory();
+	const { auth } = useAuth();
 
-  if (!auth) {
-    history.push("/");
-  }
+	if (!auth) {
+		history.push('/');
+	}
 
-  const token = JSON.parse(localStorage.getItem("token"));
+	const token = JSON.parse(localStorage.getItem('token'));
 
-  const [habits, setHabits] = useState([]);
+	const [habits, setHabits] = useState([]);
 
-  const getHabits = useCallback(() => {
-    api
-      .get("/habits/personal/", {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => setHabits(response.data));
-  }, [token]);
+	const getHabits = useCallback(() => {
+		api.get('/habits/personal/', {
+			headers: { Authorization: `Bearer ${token}` },
+		}).then((response) => setHabits(response.data));
+	}, [token]);
 
-  const [change, setChange] = useState(false);
+	const [change, setChange] = useState(false);
 
-  useEffect(() => {
-    getHabits();
-    setChange(false);
-  }, [setHabits, getHabits, change]);
+	useEffect(() => {
+		getHabits();
+		setChange(false);
+	}, [setHabits, getHabits, change]);
 
-  return (
-    <Template>
-      <HabitsContainer>
-        {habits.map((habit, index) => {
-          return (
-            <CardHabits key={index} habits={habit} setChange={setChange} />
-          );
-        })}
-      </HabitsContainer>
-    </Template>
-  );
+	return (
+		<Template>
+			<HabitsContainer>
+				{habits.map((habit, index) => {
+					return (
+						<CardHabits
+							key={index}
+							habits={habit}
+							setChange={setChange}
+						/>
+					);
+				})}
+				<ButtonAdd onClick={() => history.push('/createhabit')}>
+					Adicionar
+				</ButtonAdd>
+			</HabitsContainer>
+		</Template>
+	);
 };
 
 export default HabitsPage;
