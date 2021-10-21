f89329b3e2fe198487b206e2f300d0b46ce335a6
emanuel <emanuelzm@protonmail.com>
2021-10-21 16:04:07 -0300

    fix: conflicts resolve

--- src/components/cardGroups/index.js
+++ src/components/cardGroups/index.js
@@ -1,16 +1,34 @@
+import axios from 'axios';
+import { useHistory } from 'react-router';
+import { toast } from 'react-toastify';
 import {
 	Container,
 	NameContainer,
 	DataButtonDiv,
 	DataContainer,
+	ButtonContainer,
 } from './styles';
 
 function CardGroups({ group }) {
-	const { name, description, creator } = group;
+	const history = useHistory();
+	const { name, description, creator, id } = group;
+
+	const submitExit = (todeleteid) => {
+		const token = JSON.parse(localStorage.getItem('token'));
+		axios
+			.delete(
+				`https://kenzie-habits.herokuapp.com/groups/${todeleteid}/unsubscribe/`,
+				{
+					headers: { Authorization: `Bearer ${token}` },
+				}
+			)
+			.then((_) => toast.success('Sucesso ao sair do grup!'))
+			.catch((_) => toast.error('Erro ao sair do grupo!'));
+	};
 
 	return (
 		<Container>
-			<NameContainer>
+			<NameContainer onClick={() => history.push(`/group/${id}`)}>
 				<p> {name}</p>
 			</NameContainer>
 			<DataButtonDiv>
@@ -18,6 +36,9 @@ function CardGroups({ group }) {
 					<p>Descrição: {description}</p>
 					<p>Criador: {creator.username}</p>
 				</DataContainer>
+				<ButtonContainer onClick={() => submitExit(id)}>
+					<p>Sair</p>
+				</ButtonContainer>
 			</DataButtonDiv>
 		</Container>
 	);
