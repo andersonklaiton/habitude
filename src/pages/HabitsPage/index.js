1f54a9844aa244424cba7e7df13732668cae61ff
andersonklaiton <85939191+andersonklaiton@users.noreply.github.com>
2021-10-21 10:02:58 -0300

    Update index.js

--- src/pages/HabitsPage/index.js
+++ src/pages/HabitsPage/index.js
@@ -1,58 +1,44 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 
-import { useHistory } from 'react-router-dom';
-import CardHabits from '../../components/CardDashbord';
-import { Template } from '../../components/Template';
-import { useAuth } from '../../providers/auth';
+import { useHistory } from "react-router-dom";
+import CardHabits from "../../components/CardDashbord";
+import { Template } from "../../components/Template";
+import { useAuth } from "../../providers/auth";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
 const HabitsPage = () => {
-	const history = useHistory();
-	const { auth } = useAuth();
+  const history = useHistory();
+  const { auth } = useAuth();
 
-	const token = JSON.parse(localStorage.getItem('token'));
+  const [habits, setHabits] = useState([]);
+  const [change, setChange] = useState(false);
 
-	const [habits, setHabits] = useState([]);
+  const token = JSON.parse(localStorage.getItem("token"));
 
-	const getHabits = useCallback(() => {
-		api.get('/habits/personal/', {
-			headers: { Authorization: `Bearer ${token}` },
-		}).then((response) => setHabits(response.data));
-	}, [token]);
+  const getHabits = useCallback(() => {
+    api
+      .get("/habits/personal/", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => setHabits(response.data));
+  }, [token]);
 
+  useEffect(() => {
+    getHabits();
+    setChange(false);
+  }, [setHabits, getHabits, change]);
 
-    const [change, setChange] = useState(false);
-
-    const getHabits = useCallback(() => {
-        api.get("/habits/personal/", { headers: { Authorization: `Bearer ${token}` } })
-            .then(response => setHabits(response.data))
-    }, [token])
-
-    useEffect(() => {
-        getHabits()
-        setChange(false)
-    }, [setHabits, getHabits, change]);
-if (!auth) {
-		history.push('/');
-	}
-    return (
-        <Template>
-            {
-                habits.map((habit, index) => {
-                    return <CardHabits key={index} habits={habit} setChange={setChange} />
-                })
-            }
-
-
-	return (
-		<Template>
-			{habits.map((teste) => {
-				return <CardHabits habits={teste} />;
-			})}
-			<button></button>
-		</Template>
-	);
+  if (!auth) {
+    history.push("/");
+  }
+  return (
+    <Template>
+      {habits.map((habit, index) => {
+        return <CardHabits key={index} habits={habit} setChange={setChange} />;
+      })}
+    </Template>
+  );
 };
 
 export default HabitsPage;
