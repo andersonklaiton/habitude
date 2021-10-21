ba50a76c3492771dd9d6d2396e158f2caefc8edc
Lucas Elias <lucaselias2509@gmail.com>
2021-10-21 13:58:41 -0300

    fix: remove duplicated component

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -1,51 +1,51 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
+import { useCallback, useEffect, useState } from "react";
+import { useHistory } from "react-router";
 
-import CardGroups from '../../components/cardGroups';
-import Header from '../../components/Header';
-import PermanentSidebar from '../../components/PermanentSidebar';
-import { useAuth } from '../../providers/auth';
-import api from '../../services/api';
-import { CardsContainer } from './styles';
-
+import CardGroups from "../../components/cardGroups";
+import Header from "../../components/Header";
+import PermanentSidebar from "../../components/PermanentSidebar";
+import { useAuth } from "../../providers/auth";
+import api from "../../services/api";
+import { CardsContainer } from "./styles";
 
 function Groups() {
-	const history = useHistory();
-	const { auth } = useAuth();
-	const [groups, setGroups] = useState([]);
-	const [token] = useState(JSON.parse(localStorage.getItem('token')) || '');
+  const history = useHistory();
+  const { auth } = useAuth();
+  const [groups, setGroups] = useState([]);
+  const [token] = useState(JSON.parse(localStorage.getItem("token")) || "");
 
-	const getGroups = useCallback(() => {
-		api.get('/groups/subscriptions/', {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		}).then((response) => {
-			setGroups(response.data);
-			console.log(response.data);
-		});
-	}, [token]);
+  const getGroups = useCallback(() => {
+    api
+      .get("/groups/subscriptions/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setGroups(response.data);
+      });
+  }, [token]);
 
-	useEffect(() => {
-		getGroups();
-	});
-	if (!auth) {
-		history.push('/');
-	}
-	return (
-		<>
-			<Header />
-			<PermanentSidebar />
-			{groups.length !== 0 ? (
-				<CardsContainer>
-					{groups.map((group) => (
-						<CardGroups key={group.id} group={group} />
-					))}
-				</CardsContainer>
-			) : (
-				<div>Você não participa de nenhum grupo ainda</div>
-			)}
-		</>
-	);
+  useEffect(() => {
+    getGroups();
+  });
+  if (!auth) {
+    history.push("/");
+  }
+  return (
+    <>
+      <Header />
+      <PermanentSidebar />
+      {groups.length !== 0 ? (
+        <CardsContainer>
+          {groups.map((group) => (
+            <CardGroups key={group.id} group={group} />
+          ))}
+        </CardsContainer>
+      ) : (
+        <div>Você não participa de nenhum grupo ainda</div>
+      )}
+    </>
+  );
 }
 export default Groups;
--- src/pages/HabitsPage/index.js
+++ src/pages/HabitsPage/index.js
@@ -9,47 +9,43 @@ import api from "../../services/api";
 import { HabitsContainer } from "./styles";
 
 const HabitsPage = () => {
+  const history = useHistory();
+  const { auth } = useAuth();
 
-	const history = useHistory();
-	const { auth } = useAuth();
-	if (!auth) {
-		history.push('/');
-	}
+  if (!auth) {
+    history.push("/");
+  }
 
-	const token = JSON.parse(localStorage.getItem('token'));
+  const token = JSON.parse(localStorage.getItem("token"));
 
-	const [habits, setHabits] = useState([]);
+  const [habits, setHabits] = useState([]);
 
-	const getHabits = useCallback(() => {
-		api.get('/habits/personal/', {
-			headers: { Authorization: `Bearer ${token}` },
-		}).then((response) => setHabits(response.data));
-	}, [token]);
+  const getHabits = useCallback(() => {
+    api
+      .get("/habits/personal/", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => setHabits(response.data));
+  }, [token]);
 
-	const [change, setChange] = useState(false);
+  const [change, setChange] = useState(false);
 
-	useEffect(() => {
-		getHabits();
-		setChange(false);
-	}, [setHabits, getHabits, change]);
+  useEffect(() => {
+    getHabits();
+    setChange(false);
+  }, [setHabits, getHabits, change]);
 
-	return (
-		<Template>
-			<HabitsContainer>
-
-				{habits.map((habit, index) => {
-					return (
-						<CardHabits
-							key={index}
-							habits={habit}
-							setChange={setChange}
-						/>
-					);
-				})}
-			</HabitsContainer>
-		</Template>
-	);
-
+  return (
+    <Template>
+      <HabitsContainer>
+        {habits.map((habit, index) => {
+          return (
+            <CardHabits key={index} habits={habit} setChange={setChange} />
+          );
+        })}
+      </HabitsContainer>
+    </Template>
+  );
 };
 
 export default HabitsPage;
