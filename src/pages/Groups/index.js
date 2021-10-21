0b755161926808ec420533812d0d02f2917b0b55
emanuel <emanuelzm@protonmail.com>
2021-10-21 10:37:25 -0300

    fix: import and export useAuth() and useHistory()

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -1,14 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 
 import CardGroups from '../../components/cardGroups';
 import Header from '../../components/Header';
 import PermanentSidebar from '../../components/PermanentSidebar';
+import { useAuth } from '../../providers/auth';
 import api from '../../services/api';
 import { CardsContainer } from './styles';
 
 function Groups() {
-	const history = useHistory()
-  const {auth}=useAuth()
+	const history = useHistory();
+	const { auth } = useAuth();
 	const [groups, setGroups] = useState([]);
 	const [token] = useState(JSON.parse(localStorage.getItem('token')) || '');
 
@@ -26,9 +28,9 @@ function Groups() {
 	useEffect(() => {
 		getGroups();
 	});
-if(!auth){
-    history.push("/")
-  }
+	if (!auth) {
+		history.push('/');
+	}
 	return (
 		<>
 			<Header />
